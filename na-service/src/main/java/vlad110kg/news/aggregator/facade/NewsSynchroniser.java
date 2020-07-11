8d2aa70e9ab5fd2f483c5d6789ab570520341075
emelyanovVlad <emelyanov.vladyslav@gmail.com>
2020-07-11 12:01:27 +0700

    fixes

--- na-service/src/main/java/vlad110kg/news/aggregator/facade/NewsSynchroniser.java
+++ na-service/src/main/java/vlad110kg/news/aggregator/facade/NewsSynchroniser.java
@@ -37,40 +37,35 @@ public class NewsSynchroniser {
     private final Map<Source, AtomicInteger> sources = new HashMap<>();
 
     @Transactional
-    @Scheduled(cron = "*/10 * * * * *")
+    @Scheduled(cron = "*/20 * * * * *")
     public void sync() {
         if (sources.isEmpty()) {
             sourceService.findAll().forEach(s -> sources.put(s, new AtomicInteger(0)));
         }
         Map<Source, AtomicInteger> tempSources = new HashMap<>(sources);
-        tempSources.entrySet()
-            .parallelStream()
-            .forEach(srcE -> {
-                    Source source = srcE.getKey();
-                    AtomicInteger sourcePageNum = srcE.getValue();
+        tempSources.forEach((source, sourcePageNum) -> {
 
-                    long sourcePageAmount = sourcePageService.countBySource(source);
-                    if (sourcePageNum.get() == sourcePageAmount) {
-                        sources.remove(source);
-                        log.info("synchronisation:source:ended:{}", source.getName());
-                        return;
-                    }
+            long sourcePageAmount = sourcePageService.countBySource(source);
+            if (sourcePageNum.get() == sourcePageAmount) {
+                sources.remove(source);
+                log.info("synchronisation:source:ended:{}", source.getName());
+                return;
+            }
 
-                    PageRequest singleElementRequest = PageRequest.of(sourcePageNum.getAndIncrement(), 1);
-                    SourcePage sourcePage =
-                        sourcePageService.findFirstBySource(source, singleElementRequest).orElse(null);
-                    if (sourcePage != null) {
-                        log.info("synchronisation:started:{}", sourcePage.getUrl());
+            PageRequest singleElementRequest = PageRequest.of(sourcePageNum.getAndIncrement(), 1);
+            SourcePage sourcePage =
+                sourcePageService.findFirstBySource(source, singleElementRequest).orElse(null);
+            if (sourcePage != null) {
+                log.info("synchronisation:started:{}", sourcePage.getUrl());
 
-                        NewsSyncResult freshNotes = newsService.sync(sourcePage);
-                        log.info("synchronisation:collected:{}:{}", sourcePage.getUrl(), freshNotes.getNewsNotes());
+                NewsSyncResult freshNotes = newsService.sync(sourcePage);
+                log.info("synchronisation:collected:{}:{}", sourcePage.getUrl(), freshNotes.getNewsNotes());
 
-                        sourcePage.getReaders().forEach(r -> r.addQueueNewsNote(freshNotes.getNewsNotes()));
-                        readerService.save(sourcePage.getReaders());
-                        log.info("synchronisation:ended:{}", sourcePage.getUrl());
-                    }
-                }
-            );
+                sourcePage.getReaders().forEach(r -> r.addQueueNewsNote(freshNotes.getNewsNotes()));
+                readerService.save(sourcePage.getReaders());
+                log.info("synchronisation:ended:{}", sourcePage.getUrl());
+            }
+        });
     }
 
 }
