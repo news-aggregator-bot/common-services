46e0a7b15489fa759334f7a61df5e17bb537d7ae
emelyanovVlad <emelyanov.vladyslav@gmail.com>
2020-07-20 16:41:42 +0700

    db setup

--- scripts/send-env-config.sh
+++ scripts/send-env-config.sh
@@ -1,5 +1,6 @@
 cd ~/.ssh
 echo "SendEnv SPRING_PROFILES_ACTIVE" >> tempconfig
+echo "SendEnv MYSQL_ROOT_PASSWORD" >> tempconfig
 value=$(<config)
 echo "$value" >> tempconfig
 cp tempconfig config
--- scripts/swarm-update.sh
+++ scripts/swarm-update.sh
@@ -1,9 +1,9 @@
-wget -q $1 -O docker-stack-registry.yml
+wget -q $1 -O docker-stack-common.yml
 docker stack ls | grep -q na-common
 if [ $? -eq 0 ]; then
     echo "Updating na-registry"
-    docker service update --image docker.pkg.github.com/news-aggregator-bot/artifactory/vlad110kg.na-registry --with-registry-auth na-common_na-registry
+    docker service update --image docker.pkg.github.com/news-aggregator-bot/artifactory/vlad110kg.na-registry:latest --with-registry-auth na-common_na-registry
 else
     echo "Deploying new na-common stack"
-    docker stack deploy --compose-file docker-stack-service.yml --with-registry-auth na-common
+    docker stack deploy --compose-file docker-stack-common.yml --with-registry-auth na-common
 fi
