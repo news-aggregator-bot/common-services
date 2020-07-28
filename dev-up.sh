31f76054f703e14fd76e41559462808800fd0c37
emelyanovVlad <emelyanov.vladyslav@gmail.com>
2020-07-28 12:55:53 +0700

    volume update

--- dev-up.sh
+++ dev-up.sh
@@ -2,3 +2,4 @@
 # Starts all the services passed as arguments
 docker-compose --project-name na -f docker-compose.yml -f docker-compose.dev.yml up --no-start --build $@
 docker stack deploy --compose-file docker-compose.yml --compose-file docker-compose.dev.yml --with-registry-auth na-common
+docker service scale na-common_na-pma=0
