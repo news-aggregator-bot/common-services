f133f71b175f14cac42816058b01f18c0a253bfb
emelyanovVlad <emelyanov.vladyslav@gmail.com>
2020-08-01 12:44:22 +0700

    vlad110kg -> bepicky config-server

--- deploy-images.sh
+++ deploy-images.sh
@@ -1,3 +1,6 @@
 mvn clean package
-docker build na-registry -t docker.pkg.github.com/news-aggregator-bot/artifactory/vlad110kg.na-registry:latest
-docker push docker.pkg.github.com/news-aggregator-bot/artifactory/vlad110kg.na-registry:latest
+docker build na-registry -t docker.pkg.github.com/news-aggregator-bot/artifactory/bepicky.na-registry:latest
+docker push docker.pkg.github.com/news-aggregator-bot/artifactory/bepicky.na-registry:latest
+
+docker build na-config-server -t docker.pkg.github.com/news-aggregator-bot/artifactory/bepicky.na-config-server:latest
+docker push docker.pkg.github.com/news-aggregator-bot/artifactory/bepicky.na-config-server:latest
--- scripts/swarm-update.sh
+++ scripts/swarm-update.sh
@@ -2,7 +2,9 @@ wget -q $1 -O docker-stack-common.yml
 docker stack ls | grep -q na-common
 if [ $? -eq 0 ]; then
     echo "Updating na-registry"
-    docker service update --image docker.pkg.github.com/news-aggregator-bot/artifactory/vlad110kg.na-registry:latest --with-registry-auth na-common_na-registry
+    docker service update --image docker.pkg.github.com/news-aggregator-bot/artifactory/bepicky.na-registry:latest --with-registry-auth na-common_na-registry
+    echo "Updating na-config-server"
+    docker service update --image docker.pkg.github.com/news-aggregator-bot/artifactory/bepicky.na-config-server:latest --with-registry-auth na-common_na-config-server
 else
     echo "Deploying new na-common stack"
     docker stack deploy --compose-file docker-stack-common.yml --with-registry-auth na-common
