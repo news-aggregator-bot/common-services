2b9d5f7b41f009b92d617403220cc69ac737909d
emelyanovVlad <emelyanov.vladyslav@gmail.com>
2021-08-27 22:32:37 +0300

    delete na-config-server

--- deploy-images.sh
+++ deploy-images.sh
@@ -1,6 +1,3 @@
 mvn clean package
 docker build na-registry -t docker.pkg.github.com/news-aggregator-bot/artifactory/bepicky.na-registry:latest
 docker push docker.pkg.github.com/news-aggregator-bot/artifactory/bepicky.na-registry:latest
-
-docker build na-config-server -t docker.pkg.github.com/news-aggregator-bot/artifactory/bepicky.na-config-server:latest
-docker push docker.pkg.github.com/news-aggregator-bot/artifactory/bepicky.na-config-server:latest
